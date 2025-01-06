a0bcdfdf04dd3426e64e224a136362d43d4dc9e8
Christopher Gilbert <107435707+GoldenKnight09@users.noreply.github.com>
2025-01-06 10:16:11 -0500

    Changed dropdowns and added modal placeholder for stock groups

--- Investment_dashboard_app.py
+++ Investment_dashboard_app.py
@@ -16,25 +16,60 @@ import plotly.graph_objects as go
 investment_ticker_symbols = pd.read_excel(io = 'inputs/Investment_ticker_symbols.xlsx',
                                           sheet_name = None)
 
-stock_df = investment_ticker_symbols['Stock_list'].set_index('Stock')
-index_df = investment_ticker_symbols['Index_list'].set_index('Index')
-stock_dict_by_group = {}
-stock_group_list = list(stock_df.Group.unique())
+# look-up df for stocks:
+stock_df = investment_ticker_symbols['Stock_list'].set_index('Ticker')
+# look-up df for indices:
+index_df = investment_ticker_symbols['Index_list'].set_index('Ticker')
+index_dropdown_dict = dict((value, label) for label,value in zip(investment_ticker_symbols['Index_list']['Index'],
+                                                                 investment_ticker_symbols['Index_list']['Ticker']))
+# for some reason, dcc.Dropdown displays the dict value rather than the dict key, so invert dict
+stock_dropdown_dict_by_group = {}
+stock_group_list = list(investment_ticker_symbols['Stock_list'].Group.unique())
 for stock_group in stock_group_list:
-    stock_dict_by_group[stock_group] = stock_df.loc[stock_df['Group'] == stock_group,['Ticker']]
+    stock_group_df = investment_ticker_symbols['Stock_list'].loc[investment_ticker_symbols['Stock_list']['Group'] == stock_group,:]
+    stock_dict_by_group = dict((value, label) for label,value in zip(stock_group_df['Stock'],
+                                                                     stock_group_df['Ticker']))
+    stock_dropdown_dict_by_group[stock_group] = stock_dict_by_group
+
+index_ticker_list = index_df.index.to_list()
+
+# load stock category descriptions modal from txt file (with markdown)
+with open('inputs/stock_categories_modal.txt','r') as scm:
+    stock_cat = scm.read()
     
 app = Dash(__name__)
 server = app.server
 
 app.layout = html.Div(children = [html.Div(html.H2('Stock/Index/Commodity/Treasury Investing Dashboard',
                                                    style = {'text-align':'center'})),
-                                  html.Div(dbc.Tabs(children = [dbc.Tab(children = [dbc.Col(children = [sup_func.generate_dropdown_menu(stock_dict_by_group[group_name], group_name) for group_name in stock_group_list])],
-                                                                        # need to group stocks and make dropdowns for each in a column or row (too many entries for a single dbc dropdown)
+                                  html.Div(dbc.Tabs(children = [dbc.Tab(children = [dbc.Button('Click here for an explanation of the stock categories',
+                                                                                               id = 'open_stock_cat_def_modal'),
+                                                                                    dbc.Modal(children = [dbc.ModalHeader(dbc.ModalTitle('Stock Category Definitions'),
+                                                                                                                          close_button = False),
+                                                                                                          dbc.ModalBody(dbc.Container(children = [dcc.Markdown(stock_cat)])),
+                                                                                                          dbc.ModalFooter(dbc.Button('Close',
+                                                                                                                                     id = 'close_stock_cat_def_modal',
+                                                                                                                                     className = 'ml-auto'))],
+                                                                                              id = 'stock_cat_def_modal',
+                                                                                              size = 'lg',
+                                                                                              scrollable = True),
+                                                                                    dbc.Col(children = [dcc.Dropdown(options = stock_group_list,
+                                                                                                                     searchable = True,
+                                                                                                                     placeholder = 'Select a stock group...',
+                                                                                                                     id = 'stock_group_dropdown_menu'),
+                                                                                                        dcc.Dropdown()],
+                                                                                            width = 2),
+                                                                                    dbc.Col(children = [dbc.Label(id = 'stock_ticker_test')])],
                                                                         label = "Stocks & ETF's",
                                                                         style = {'margin-left':'3px'}),
-                                                                dbc.Tab(children = [dbc.Col(children = dbc.DropdownMenu(label = 'Select an Index...',
-                                                                                                                        id = 'index_ticker_dropdown',
-                                                                                                                        children = [sup_func.generate_dropdown_menu_item(dropdown_item) for dropdown_item in index_df.index.to_list()]))],
+                                                                dbc.Tab(children = [dbc.Col(children = dcc.Dropdown(options = index_dropdown_dict,
+                                                                                                                    searchable = True,
+                                                                                                                    placeholder = 'Select an index...',
+                                                                                                                    id = 'index_dropdown_menu'),
+                                                                                            width = 2),
+                                                                                    # might need to wrap the Col's in a dbc.Row
+                                                                                    dbc.Col(children = [dbc.Label(id = 'index_ticker_test')],
+                                                                                            width = 4)],
                                                                         label = 'Indices',
                                                                         style = {'margin-left':'3px'}),
                                                                 dbc.Tab(children = [],
@@ -44,6 +79,28 @@ app.layout = html.Div(children = [html.Div(html.H2('Stock/Index/Commodity/Treasu
                                                                         label = 'US Treasuries',
                                                                         style = {'margin-left':'3px'})],
                                                     style = {'margin-left':'3px'}))])
+
+@app.callback(Output(component_id='stock_cat_def_modal', component_property='is_open'),
+              Input(component_id='open_stock_cat_def_modal',component_property='n_clicks'),
+              Input(component_id='close_stock_cat_def_modal',component_property='n_clicks'),
+              State(component_id='stock_cat_def_modal',component_property='is_open'))
+
+def toggle_stock_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+@app.callback(Output(component_id='stock_ticker_test',component_property='children'),
+              Input(component_id='stock_group_dropdown_menu',component_property='value'))
+
+def stock_ticker_test_display(stock_group_value):
+    return stock_group_value
+
+@app.callback(Output(component_id='index_ticker_test',component_property='children'),
+              Input(component_id='index_dropdown_menu',component_property='value'))
+
+def index_ticker_test_display(index_value):
+    return index_value
 
 if __name__ == '__main__':
     app.run(debug = False)
